f60b666f5b6eaa91c69db4983675785c49c45619
Camp4er <camp4ercode@gmail.com>
2025-03-03 12:55:21 +0530

    rough navbar structure and next-themes installed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+"use client";
+import Link from "next/link";
+import { useTheme } from "next-themes";
 
 const Navbar = () => {
+  const { theme, setTheme } = useTheme();
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <nav className="flex justify-between p-4 bg-gray-200 dark:bg-gray-900">
+      <Link href="/" className="text-xl font-bold">
+        MemeVerse
+      </Link>
+      <div className="space-x-4">
+        <Link href="/explore">Explore</Link>
+        <Link href="/upload">Upload</Link>
+        <Link href="/leaderboard">Leaderboard</Link>
+        <button
+          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+          className="ml-4"
+        >
+          🌙 / ☀️
+        </button>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import "@/styles/globals.css";
+import "../styles/globals.css";
+import { ThemeProvider } from "next-themes";
+import Navbar from "../components/Navbar";
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider attribute="class">
+      <Navbar />
+      <main className="p-4">
+        <Component {...pageProps} />
+      </main>
+    </ThemeProvider>
+  );
 }
